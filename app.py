fe254377453611a5e06574658fcda88c43c6ed5a
codereber <jamesmpyle@gmail.com>
2020-11-02 18:39:51 -0500

    update

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask, jsonify, request, redirect, url_for, render_template
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+from flask_wtf import FlaskForm
+from wtforms import SelectField
 import pymysql
 import secrets
+from datetime import date
+
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,6 +18,18 @@ app.config['SECRET_KEY']='SuperSecretKey'
 #app.config['SQLALCHEMY_DATABASE_URI'] = conn
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///extsql.db"            
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+
+db = SQLAlchemy(app)
+
+class aggregate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    uuid = db.Column(db.String(255))
+    name = db.Column(db.String(255))
+
+class aggregatecapacityhistoryyearview(db.Model):
+    aggregateid = db.Column(db.Integer, primary_key=True)
+    periodEndTime = db.Column(db.String(255))
+    UsedSum = db.Column(db.BigInteger)
 
 
 @app.route('/')
@@ -27,3 +43,22 @@ def map():
 @app.route('/storage')
 def storage():
     return render_template('storage.html')
+
+@app.route('/api/all/<aggr>')
+def all(aggr):
+    all = db.session.query().with_entities(aggregate.name,aggregatecapacityhistoryyearview.periodEndTime,aggregatecapacityhistoryyearview.UsedSum).join(aggregatecapacityhistoryyearview,aggregate.id==aggregatecapacityhistoryyearview.aggregateid)\
+        .filter(aggregatecapacityhistoryyearview.periodEndTime>date(2020,5,1),aggregatecapacityhistoryyearview.periodEndTime<date(2020,8,18))\
+            .order_by(aggregate.name.asc(),aggregatecapacityhistoryyearview.periodEndTime.asc()).all()  
+
+    aggrArray = []
+
+    for aggregate.name,aggregatecapacityhistoryyearview.periodEndTime,aggregatecapacityhistoryyearview.UsedSum in all:
+        aggrObj = {}
+        aggrObj['aggr_name'] = aggregate.name
+        aggrObj['date'] = aggregatecapacityhistoryyearview.periodEndTime
+        aggrObj['Capacity'] = aggregatecapacityhistoryyearview.UsedSum
+        aggrArray.append(aggrObj)
+
+    return jsonify([aggrArray]) 
+
+
