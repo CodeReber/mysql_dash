5641a56aa0ec71424a08a614f129a59f559af6e4
codereber <jamesmpyle@gmail.com>
2020-11-12 06:57:18 -0500

    update

--- static/test.js
+++ static/test.js
@@ -1,18 +1,17 @@
+let aggrname_select = document.getElementById('aggrname');
+console.log(aggrname_select)
 fetch('/api/chart/' + 24).then(function(response){
     response.json().then(function(data){
         // console.log(data);
-        var arr = [];
-        var arr1 = [];
+        var capacities = [];
+        var dates = [];
         for (let obj of data.results){
-               arr.push(obj.Capacity);
-               arr1.push(obj.Date);
+               capacities.push(obj.Capacity);
+               dates.push(obj.Date);
         }
-    //     x: ["beer", "wine", "martini", "margarita",
-    //     "ice tea", "rum & coke", "mai tai", "gin & tonic"],
-    //   y: [22.7, 17.1, 9.9, 8.7, 7.2, 6.1, 6.0, 4.6],
 var trace1 = {
-    x: arr,
-    y: arr1,
+    x: capacities,
+    y: dates,
     type: "line"
   };
   
